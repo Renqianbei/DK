4d43f16cf232a480366b5d28c3168b88c93335cd
predecessor <renqianbei@sina.cn>
2018-07-11 23:35:50 +0800

    提交

---
+++ LOCATION/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOCATION
+//
+//  Created by MR on 2018/3/21.
+//  Copyright © 2018年 MRXIER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LOCATION/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOCATION
+//
+//  Created by MR on 2018/3/21.
+//  Copyright © 2018年 MRXIER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
